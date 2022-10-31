23a609778837c42003c2e30461ebc226fc44abe8
beanb2 <brennan.bean@usu.edu>
2022-10-31 17:00:05 -0600

    headed home

--- data-raw/cereal_dat_prep.R
+++ data-raw/cereal_dat_prep.R
@@ -3,3 +3,8 @@ cereal <- data.table::fread("https://www.hawkeslearning.com/Statistics/datasets/
 cereal <- as.data.frame(cereal)
 colnames(cereal)[14:15] <- c("Weight", "Cups")
 usethis::use_data(cereal, overwrite = TRUE)
+
+cereal_infected <- cereal
+cereal_infected$Sugars[27] <- cereal_infected$Sugars[27]*10
+
+usethis::use_data(cereal_infected, overwrite = TRUE)
